5a46b65c7522afc760ded1740c74fb866d7ef1d7
iGus12 <ag.varas@duocuc.cl>
2026-05-06 09:32:02 -0400

    Add files via upload

---
+++ src/test/java/ms_usuarios/ms_usuario/MsUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package ms_usuarios.ms_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
