dad5abd34a8210f2c886c6bd557c95ac4e14d880
iGus12 <ag.varas@duocuc.cl>
2026-05-11 22:26:28 -0400

    cambio

--- src/main/java/ms_usuarios/ms_usuario/controller/UsuarioController.java
+++ src/main/java/ms_usuarios/ms_usuario/controller/UsuarioController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/usuarios")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UsuarioController {
 
     @Autowired
